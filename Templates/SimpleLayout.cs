96a5e4fdd529cecc57f9bb9acdc21b1f7d00af80
johnmott59 <johnkmott@gmail.com>
2018-01-28 19:07:26 -0600

    Updates
    Updates

--- Templates/SimpleLayout.cs
+++ Templates/SimpleLayout.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -141,4 +141,3 @@ namespace ShapeTemplateLibUnitTest
 
     }
 }
-
--- Templates/SingleRoomTest.cs
+++ Templates/SingleRoomTest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -34,4 +34,3 @@ namespace ShapeTemplateLibUnitTest
 
     }
 }
-
